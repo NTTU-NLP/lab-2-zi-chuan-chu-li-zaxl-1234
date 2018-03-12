93313d25091ffef6e05c53e205f2827959a625e4
zaxl <a0987730876@gmail.com>
2018-03-12 17:21:29 +0800

    lab 2 繳交

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\admin\Desktop\lab-2-zi-chuan-chu-li-zaxl-1234\Data\Input.txt");
+            String line = null;
+            while ((line = file.ReadLine()) != null)
+            {
+                var chars = line.ToCharArray();
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (chars[i].Equals('.') || chars[i].Equals('?') || chars[i].Equals('!')) 
+                    { 
+                        Console.WriteLine(chars[i]); 
+                    }
+                    else 
+                    { 
+                        Console.Write(chars[i]); 
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
+
